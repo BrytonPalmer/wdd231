fea00163175e6850af5f174306090e6afd6fc3a9
brytonpalmer <bpalmer2@byupathway.edu>
2025-11-08 12:47:04 -0700

    nav wayfinding

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
     nav.classList.toggle("show");
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".nav-link");
+  const currentPath = window.location.pathname.split("/").pop();
+
+  links.forEach(link => {
+    const linkPath = link.getAttribute("href").split("/").pop();
+    if (linkPath === currentPath) {
+      link.classList.add("active");
+    }
+  });
+});
